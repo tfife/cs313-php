460a4886e26a55d4d30f4cd6479b30b2132090c0
Tori Fife <torifife@gmail.com>
2019-09-26 20:07:20 -0600

    added assignments page

---
+++ web/assignments.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="02teach.css">
+    <meta charset="UTF-8">
+    <title>02Teach</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="CS-313 02 Teach Assignment">
+    <script src="02teach.js"></script>
+</head>
+
+<body>
+    <header>
+    </header>
+
+    <div class="flexbox">
+        <div class="box" id="box1">I'm a div!<br><input id="color" type="text"></input><button onclick="changeColor()">Change Color</button></div>
+        <div class="box">I'm another div!</div>
+        <div class="box">I'm the best div!</div>
+    </div>
+
+    <div class="flexbox">
+        <div class="small"><button id="prompt" onclick=clicked()>Click Me!</button></div>
+        <div class="small" id="clickMessage" style="font-size: 30px; color:green"></div>
+    </div>
+
+
+    <footer>
+        Website created by Tori Fife.
+    </footer>
+</body>
+
+</html>

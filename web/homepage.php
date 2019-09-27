30fb8c97b13e9da173c8ff01a190892129ee8d37
Tori Fife <torifife@gmail.com>
2019-09-26 20:19:10 -0600

    added stylesheet and homepage

---
+++ web/homepage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="fife_gen.css">
+    <meta charset="UTF-8">
+    <title>02Teach</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Homepage">
+</head>
+
+<body>
+    <header>
+        <h1>Tori Fife</h1>
+    </header>
+
+    <div class="bodyBox">
+      <h2>About Me:</h2>
+    </div>
+
+    <footer>
+        Website created by Tori Fife.
+    </footer>
+</body>
+
+</html>

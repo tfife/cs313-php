2b886a86f76dc529aa0d1641b41f1f9374bed9b5
Tori Fife <torifife@gmail.com>
2019-09-26 20:35:36 -0600

    just testing things out

--- web/assignments.php
+++ web/assignments.php
@@ -2,29 +2,19 @@
 <html lang="en-us">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="02teach.css">
+    <link rel="stylesheet" type="text/css" href="fife_gen.css">
     <meta charset="UTF-8">
     <title>02Teach</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="CS-313 02 Teach Assignment">
-    <script src="02teach.js"></script>
+    <meta name="description" content="Index of Assignments">
 </head>
 
 <body>
     <header>
+        <h1>CS 313 Assignments</h1>
     </header>
 
-    <div class="flexbox">
-        <div class="box" id="box1">I'm a div!<br><input id="color" type="text"></input><button onclick="changeColor()">Change Color</button></div>
-        <div class="box">I'm another div!</div>
-        <div class="box">I'm the best div!</div>
-    </div>
-
-    <div class="flexbox">
-        <div class="small"><button id="prompt" onclick=clicked()>Click Me!</button></div>
-        <div class="small" id="clickMessage" style="font-size: 30px; color:green"></div>
-    </div>
-
+    <div class="bodyBox">links will appear here when they are available</div>
 
     <footer>
         Website created by Tori Fife.

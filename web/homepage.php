a853c123d6f072d1549c94ca7190408aa76c0c7c
Tori Fife <torifife@gmail.com>
2019-09-28 21:00:19 -0600

    add stuff

--- web/homepage.php
+++ web/homepage.php
@@ -29,10 +29,10 @@
             <img class="medPic" src="images/create_cupcakes.jpg" alt="cupcakes">
         </div>
         <div>
-            <img class="medPic" src="images/create_clay.jpg" alt="clay gnome">
+            <div class="medTextbox">One of my favorite things about myself is my propensity for creativity. Ever since I was a little kid, I loved writing and wanted to be an author. I liked drawing and painting and wanted to be an artist. As I got older, I thought I might want to be an architect or an interior designer.</div>
         </div>
         <div>
-            <div class="medTextbox">One of my favorite things about myself is my propensity for creativity. Ever since I was a little kid, I loved writing and wanted to be an author. I liked drawing and painting and wanted to be an artist. As I got older, I thought I might want to be an architect or an interior designer.</div>
+            <img class="medPic" src="images/create_clay.jpg" alt="clay gnome">
         </div>
     </div>
     <div class="dioBox" style="background-color: darkmagenta">
@@ -46,20 +46,19 @@
     </div>
     <div class="dioBox" style="background-color: gold"> 
         <div>
-            <div class="medTextbox">Something I've been doing for about eight years now is cake decorating. While I was <em>okay</em> at it from the beginning, it was amazing to look back to the beginning after a few years and to see how far I'd come. I still like to learn new techniques and even make up some of my own, like the ducks I piped onto this cake.</div>
+            <img class="medPic" src="images/create_crochet.jpg" alt="crochet dragon">
         </div>
         <div>
             <img class="medPic" src="images/create_ducks.jpg" alt="duck cake">
         </div>
         <div>
-            <div class="medTextbox">One <em>new</em> thing I started about three years ago is computer programming. After a single one-credit Python class for non-majors, I changed my major from English to Software Engineering. It was a totally new world, and I've never stopped feeling overwhelmed, but I know that with practice, I'll be able to look back and see how far I've come. Programming is just another way to express my creativity, while at the same time thinking analytically and (hopefully) making some money. :)</div>
+            <div class="medTextbox">Something I've been doing for about eight years now is cake decorating. While I was <em>okay</em> at it from the beginning, it was amazing to look back to the beginning after a few years and to see how far I'd come. I still like to learn new techniques and even make up some of my own, like the ducks I piped onto this cake.</div>
         </div>
-
     </div>
 
     <div class="dioBox" style="background-color: indigo">
         <div>
-            <img class="medPic" src="images/create_crochet.jpg" alt="crochet dragon">
+            <div class="medTextbox">When I changed my major from English to Software Engineering, my only experience with programming was a one-credit Python class for non-majors. I was brand new to the world of computers, which is <em>still</em> a little overwhelming. But I love programming because it's a great way to be creative while also getting to think analytically and (hopefully) make a little bit of money. :)</div>
         </div>
         <div>
             <img class="medPic" src="images/create_pumpkin.jpg" alt="Pumpkin of the Restoration">
@@ -68,9 +67,14 @@
             <img class="medPic" src="images/create_cookies.jpg" alt="Halloween cookies">
         </div>
     </div>
+    <div class="dioBox" style="background-color: magenta">
+        <div>
+            <img class="medPic" src="images/create_tape.jpg" alt="Duct Tape Jack-o-lantern">
+        </div>
+    </div>
 
     <footer>
-        Website created by Tori Fife.
+        Website created by Tori Fife. 9/2019.
     </footer>
 </body>
 

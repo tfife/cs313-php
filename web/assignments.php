b7f6ea8e9e95747401f35829f6eae6668c62fce3
Tori Fife <torifife@gmail.com>
2019-09-28 18:28:55 -0600

    add link boxes to assignments page

--- web/assignments.php
+++ web/assignments.php
@@ -15,7 +15,13 @@
     </header>
 
     <div class="title">CS 313 Assignments</div>
-    <div class="bodyBox">links will appear here when they are available</div>
+    <div class="bodyBox">
+        Note: links will appear in order from newest to oldest.
+        <div class="flexgroup">
+            <div class="linkBox"><a href="homepage.php">Homepage<span class="small"><br>9/28/19</span></a></div>
+            <div class="linkBox"><a href="hello.php">Hello World!<span class="small"><br>9/21/19</span></a></div>
+        </div>
+    </div>
 
     <footer>
         Website created by Tori Fife.

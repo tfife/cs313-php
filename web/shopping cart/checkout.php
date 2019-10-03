740a278ff6938b8f81659672935081fbb7d755ea
Tori Fife <torifife@gmail.com>
2019-10-03 09:09:03 -0600

    add files for shopping cart assignment

---
+++ web/shopping cart/checkout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="fife_gen.css">
+    <meta charset="UTF-8">
+    <title>Checkout</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+</head>
+
+<body background="images/triangles.jpg">
+    <header>
+        <?php include ("navbar_gen.php"); ?>
+    </header>
+  
+    <div class="title">Checkout</div>
+
+
+    <footer>
+        Website created by Tori Fife. 9/2019.
+    </footer>
+</body>
+
+</html>

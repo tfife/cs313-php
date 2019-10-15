b0c5c0dfc04c5e88d3b8c0dcc8ee27c15effbc6e
Tori Fife <torifife@gmail.com>
2019-10-15 15:06:51 -0600

    Co-authored-by: FiscalMonkey <mor16112@byui.edu> Co-authored-by: jordanburdett <jordan@burdett.us>

--- web/liveshare.php
+++ web/liveshare.php
@@ -1,4 +1,8 @@
 <?php
+
+/***********************************************8
+ * http://whispering-thicket-79511.herokuapp.com/liveshare.php
+ */
 session_start();
 try {
     $dbUrl = getenv('DATABASE_URL');
@@ -32,19 +36,38 @@ try {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: whitesmoke;
+            color: white;
+            text-align: center;
+        }
+    </style>
 </head>
 
 <header>
 </header>
 <div class="container">
     <div class="jumbotron">
+        <form>
+            <label for="scripture" class="h3">
+                <select id="scripture" class="form-control">
+                    <?php foreach ($db->query('SELECT book, chapter, verse FROM Scriptures ORDER BY book') as $row) {
+                        echo '<option value="' . $row["year"] . '">' . $row["year"] . '</option>';
+                    } ?>
+                </select>
+        </form>
     </div>
 </div>
 
 
-<footer>
+<div class="footer">
     Website created by the coolest team ever.
-</footer>
+</div>
 </body>
 
 </html>
